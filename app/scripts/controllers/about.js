79728753d9882098bdae674eef92fb057522dbe9
Klaus Wenger <kjwenger@yahoo.com>
2015-12-09 11:57:40 +0100

    initial load

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sandboxangularjsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sandboxangularjsApp
+ */
+angular.module('sandboxangularjsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
